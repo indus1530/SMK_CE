9040ad261ffe4dfe6d0f883e3e9e6800f696bed2
SAJID LATIF <indus1530@gmail.com>
2021-05-08 02:09:03 +1400

    BuildCorrections

--- app/src/main/java/edu/aku/hassannaqvi/smk_ce/database/CreateTable.kt
+++ app/src/main/java/edu/aku/hassannaqvi/smk_ce/database/CreateTable.kt
@@ -4,7 +4,7 @@ import edu.aku.hassannaqvi.smk_ce.contracts.*
 import edu.aku.hassannaqvi.smk_ce.models.*
 
 object CreateTable {
-    const val PROJECT_NAME = "NAUNEHAL2021"
+    const val PROJECT_NAME = "SMKCE"
     const val DATABASE_NAME = "$PROJECT_NAME.db"
     const val DATABASE_COPY = "${PROJECT_NAME}_copy.db"
     const val DATABASE_VERSION = 1
