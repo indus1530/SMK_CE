f56f0d35f4450ce1cd819513ad569c7339da6329
SAJID LATIF <indus1530@gmail.com>
2021-05-07 13:04:50 +0500

    strings.xml

--- app/src/main/java/edu/aku/hassannaqvi/smk_ce/ui/sections/Section01.kt
+++ app/src/main/java/edu/aku/hassannaqvi/smk_ce/ui/sections/Section01.kt
@@ -2,6 +2,7 @@ package edu.aku.hassannaqvi.smk_ce.ui.sections
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import edu.aku.hassannaqvi.smk_ce.R
 
 class Section01 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
