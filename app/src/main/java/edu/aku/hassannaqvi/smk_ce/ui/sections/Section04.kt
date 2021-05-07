7b0f6d4b9068b2778527d9d27ab1d54e49176653
SAJID LATIF <indus1530@gmail.com>
2021-05-08 01:49:27 +1400

    Section04.kt

---
+++ app/src/main/java/edu/aku/hassannaqvi/smk_ce/ui/sections/Section04.kt
@@ -0,0 +1,42 @@
+package edu.aku.hassannaqvi.smk_ce.ui.sections
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import edu.aku.hassannaqvi.smk_ce.R
+import edu.aku.hassannaqvi.smk_ce.databinding.ActivitySection04Binding
+
+class Section04 : AppCompatActivity() {
+
+    lateinit var bi: ActivitySection04Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section04)
+        bi.callback = this
+    }
+
+
+    fun BtnContinue(view: View) {
+        /*if (!formValidation()) return
+        try {
+            saveDraft()
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        if (UpdateDB()) {
+            finish()
+            startActivity(Intent(this, SectionG415Activity::class.java))
+        } else {
+            Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show()
+        }*/
+    }
+
+
+    fun BtnEnd(view: View) {
+        //openSectionMainActivity(this, "G")
+    }
+
+
+}
