9689577f3f182f47ebac90820edb002ac863f8f7
SAJID LATIF <indus1530@gmail.com>
2021-05-08 01:36:06 +1400

    Section01.kt & Section02.kt

--- app/src/main/java/edu/aku/hassannaqvi/smk_ce/ui/sections/Section01.kt
+++ app/src/main/java/edu/aku/hassannaqvi/smk_ce/ui/sections/Section01.kt
@@ -1,12 +1,41 @@
 package edu.aku.hassannaqvi.smk_ce.ui.sections
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import edu.aku.hassannaqvi.smk_ce.R
+import edu.aku.hassannaqvi.smk_ce.databinding.ActivitySection01Binding
 
 class Section01 : AppCompatActivity() {
+
+    lateinit var bi: ActivitySection01Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_section01)
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section01)
+        bi.callback = this
     }
+
+
+    fun BtnContinue(view: View) {
+        /*if (!formValidation()) return
+        try {
+            saveDraft()
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        if (UpdateDB()) {
+            finish()
+            startActivity(Intent(this, SectionG415Activity::class.java))
+        } else {
+            Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show()
+        }*/
+    }
+
+
+    fun BtnEnd(view: View) {
+        //openSectionMainActivity(this, "G")
+    }
+
 }
---
+++ app/src/main/java/edu/aku/hassannaqvi/smk_ce/ui/sections/Section02.kt
@@ -0,0 +1,12 @@
+package edu.aku.hassannaqvi.smk_ce.ui.sections
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.aku.hassannaqvi.smk_ce.R
+
+class Section02 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_section02)
+    }
+}
